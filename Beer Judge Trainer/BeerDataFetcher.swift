6b425269462789d4b6b13fa2ebcd5a9b88689342
Nathan T. Baker <nathantbaker@gmail.com>
2017-03-14 10:49:58 -0500

    set up home and results views with connected buttons and skeleton models and controllers

---
+++ Beer Judge Trainer/BeerDataFetcher.swift
@@ -0,0 +1,11 @@
+//
+//  BeerDataFetcher.swift
+//  Beer Judge Trainer
+//
+//  Created by Nate on 3/14/17.
+//  Copyright © 2017 Nathan T. Baker. All rights reserved.
+//
+
+import Foundation
+
+//this one
---
+++ Beer Judge Trainer/ScoresheetComparisonBot.swift
@@ -0,0 +1,9 @@
+//
+//  ScoresheetComparisonBot.swift
+//  Beer Judge Trainer
+//
+//  Created by Nate on 3/14/17.
+//  Copyright © 2017 Nathan T. Baker. All rights reserved.
+//
+
+import Foundation
