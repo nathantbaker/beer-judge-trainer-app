4904fbd10737f174f4beec37ee68dbe03f4db82e
Nathan T. Baker <nathantbaker@gmail.com>
2017-03-18 00:20:07 -0500

    set how photos are used in info.plist

--- Beer Judge Trainer/ResultsController.swift
+++ Beer Judge Trainer/ResultsController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ResultsController: UIViewController {
     
+    @IBAction func takeScreenshot(_ sender: Any) {
+        //Create the UIImage
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        //Save it to the camera roll
+        UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
