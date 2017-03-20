e22058e94f29399e9d318699fee2193cbc5683e3
Nathan T. Baker <nathantbaker@gmail.com>
2017-03-20 09:38:26 -0500

    beer range description updated

--- Beer Judge Trainer/ResultsLanguageBot.swift
+++ Beer Judge Trainer/ResultsLanguageBot.swift
@@ -17,3 +17,12 @@ import Foundation
 //  4-6:  Ok            yellow
 //  6-8:  Not so close  orange
 //  8-*:  Way off       red
+//
+
+
+//Outstanding (45 - 50):  World-class example of style.
+//Excellent      (38 - 44):  Exemplifies style well, requires minor fine-tuning.
+//Very Good    (30 - 37):  Generally within style parameters, some minor flaws.
+//Good            (21 - 29):  Misses the mark on style and/or minor flaws.
+//Fair              (14 - 20):  Off flavors/aromas or major style deficiencies. Unpleasant.
+//Problematic  (00 - 13):  Major off flavors and aromas dominate. Hard to drink.
